6e5193844d4333aea7a59a786e17c079450fd1a9
Yohann <yohanndossantos.pro@gmail.com>
2022-02-14 12:46:29 +0100

    test

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,4 +2,37 @@ package test;
 
 public class Test1 {
 
+	private int A = 1;
+	private int B = 2;
+	
+	
+	public void Test2 (){
+		this.A = A;
+		this.B = B;
+		
+	}
+
+
+	public int getA() {
+		return A;
+	}
+
+
+	public void setA(int a) {
+		A = a;
+	}
+
+
+	public int getB() {
+		return B;
+	}
+
+
+	public void setB(int b) {
+		B = b;
+	}
+	
+	public String toString() {
+		return "resultat" + getA() + getB();
+	}
 }
