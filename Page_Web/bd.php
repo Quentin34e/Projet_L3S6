04138c70b22bad87236f7b2fd95d60b2ecf7e907
maxime <maximele77@live.fr>
2022-04-09 20:00:27 +0200

    modif

--- Page_Web/bd.php
+++ Page_Web/bd.php
@@ -1,11 +1,8 @@
 <?php
 	function getBD(){
-		try{
+		
 			$bdd = new PDO('mysql:host=localhost;dbname=addictab2;charset=utf8', 'root', 'root');
 			return $bdd;
-		}
-		catch(Exception $e){
-			die('Une erreur a été trouvée : ' . $e->getMessage());
 		}
 ?>
 
