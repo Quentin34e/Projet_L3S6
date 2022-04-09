f8fed3a4c0743fa6ca8b7cd68cb6f378c2f127be
maxime <maximele77@live.fr>
2022-04-09 19:20:20 +0200

    modification fichier et suppression fichier inutile

--- Page_Web/questions/myQuestionsAction.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require('../bd.php');
-
-$getAllMyQuestions = $bdd->prepare('SELECT id, titre, description FROM questions WHERE id_autheur = ? ORDER BY id DESC');
-$getAllMyQuestions->execute(array($_SESSION['id_utilisateur']));
