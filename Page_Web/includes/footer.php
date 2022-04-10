b0472ec9e44c8237c13c12606f6289a1763c2735
maxime <maximele77@live.fr>
2022-04-10 02:32:14 +0200

    jpgraph

---
+++ Page_Web/includes/footer.php
@@ -0,0 +1,8 @@
+<footer><!--PIED DE PAGE-->
+            <br><br>
+			<h2 align="center">Contact</h2>
+				<p align="center"><a href="tel:06XXXXXXXX"><img src="../img/Logo_Telephone.jpg" alt="image téléphone" position="relative" width="60px" margin=" 15px 15px 15px 15px"top="5px" /></a>
+				<a href="mailto:addictab123@gmail.com"><img src="../img/email.jpg" alt="image enveloppe" position="relative" width="60px" margin=" 15px 15px 15px 15px"top="5px"/></a>
+				<a href="https://www.facebook.com/profile.php?id=100078106309466"><img src="../img/Logo_facebook.png" alt="logo facebook" position="relative" width="60px" margin=" 15px 15px 15px 15px"top="5px"/></a>
+				
+ </footer> <!--FIN PIED DE PAGE-->
